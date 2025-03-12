3b61015f57d6d7d7a61371bae0c3b5a1c6224c62
Alex C <alexcarson1379@gmail.com>
2025-03-12 11:43:30 -0600

    Rev 1

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
 /*
+Alexander S. Carson
+3/12/2025
+Purpose: This program is designed to read in a file of data and output a file of data.
+prepared for CSCI 2251.
+Sources:
+*/
+/*
 This code is provided to give you a
 starting place. It should be modified.
 No further imports are needed.
@@ -7,32 +14,42 @@ answer the following questions:
 
 Q1: Car and Engine are related
 by which, Inheritance or Composition?
-
+Composition(Has-A relationship)
 Q2: Color and Red are related
 by which, Inheritance or Composition?
-
+Inheritance(Is-A relationship)
 Q3: Shirt and Clothing are related
 by which, Inheritance or Composition?
-
+Inheritance(Is-A relationship)
 Q4: Furniture and Desk are related
 by which, Inheritance or Composition?
-
+Inheritance(Is-A relationship)
 Q5: CellPhone and Battery are related
 by which, Inheritance or Composition?
-
+Composition(Has-A relationship)
 */
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.File; //Import the File class to read files
+import java.io.FileNotFoundException;
+import java.io.FileWriter; //Import the FileWriter class to write to files
+import java.io.IOException; //Import the IOException class to handle errors
+import java.util.Scanner; //Import the Scanner class to read text files
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		
-		System.out.println();
-		
+		try{
+			File inputTxtFile = new File("hr.txt"); // Create a File object
+			Scanner txtFileReader = new Scanner(inputTxtFile); // Create a Scanner object
+			txtFileReader.close(); // Close the scanner to avoid a resource leak
+		}
+		catch(FileNotFoundException e) {
+			System.out.println(e);
+		}
+	public class Person {
+			
+	}
+
 		/*
 		// Don't overcomplicate the data
 		// reading. After skipping the
